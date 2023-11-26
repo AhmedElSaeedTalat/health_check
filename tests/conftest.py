53d8437fec5cb9242240a6c9f2dc2c39d2595ddd
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-11-26 00:03:20 +0000

    test register

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from healthapp import create_app, db
 from healthapp.models.users import User
 from healthapp.models.drugs import Drug
 from healthapp.models.articles import Article
+import os
 
 
 @pytest.fixture()
@@ -13,6 +14,9 @@ def app():
     app = create_app()
     with app.app_context():
         db.create_all()
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SERVER_NAME'] = 'localhost'
+        app.config['TESTING'] = True
     yield app
     with app.app_context():
         db.drop_all()
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ write test cases """
+from healthapp.models.users import User
+from flask_wtf.csrf import generate_csrf
+from healthapp.views.forms import Registration
+from flask import url_for
 
 
 def test_home(client):
     """ test home page """
     response = client.get('/')
     assert b'<link type="text/css" rel="stylesheet" href="/static/style/main.css">' in response.data
+
+
+def test_registration(client, app):
+    """ test registration function """
+    with app.app_context():
+        data = {
+                'username': 'Jordan',
+                'email': 'jordan@mail.com',
+                'password': '123456',
+                'confirm_password': '123456',
+                }
+        response = client.post(url_for('user_views.register'), data=data, follow_redirects=True)
+        assert response.status_code == 200
+        users = User.query.all()
+        assert len(users) == 1
+        user = User.query.first()
+        assert user.email == 'jordan@mail.com'
